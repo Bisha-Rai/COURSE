e040d48ae4bc7f9eb10e3beb7a0be9ffbf43c710
Bisha-Rai <bishalchamling14@gmail.com>
2025-08-10 23:49:31 +0545

    not final

--- EXAMS/DSA/QUEUE And LINKED LIST/SinglyLinkedList.c
+++ EXAMS/DSA/QUEUE And LINKED LIST/SinglyLinkedList.c
@@ -6,13 +6,31 @@ struct node
     struct node *next;
 };
 struct node *head = NULL;
-void insert(int data)
+void insert_beginning(int data)
 {
     struct node*newnode=(struct node*)malloc(sizeof(struct node));
     newnode->info=data;
     newnode->next=head;
     head=newnode;
     return;
+}
+void insert_end(int d)
+{
+    struct node*newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->info=d;
+    newnode->next=NULL;
+    if(head==NULL)
+    {   
+
+        head=newnode;
+        return;
+    }
+    struct node *ptr=head;
+    while(ptr->next!=NULL);
+    {
+        ptr=ptr->next;
+        ptr->next=newnode;
+    }
 }
 void display()
 {
@@ -24,7 +42,7 @@ void display()
     else
     {
         printf("the list : \n");
-        while(ptr!=NULL)
+        while(ptr->next!=NULL)
         {
             printf("%d\t",ptr->info);
             ptr=ptr->next;
@@ -40,19 +58,25 @@ int main()
     {
     printf("enter 1 to insert from beginning\n");
     printf("enter 2 to display\n");
-    printf("enter 3 to exit\n");
+    printf("enter 3 to insert from end\n");
+    printf("enter 4 to exit\n");
     scanf("%d",&c);
     switch (c)
     {
         case 1:
             printf("enter the data\n");
             scanf("%d",&data);
-            insert(data);
+            insert_beginning(data);
             break;
         case 2:
             display();
             break;
         case 3:
+            printf("enter the data\n");
+            scanf("%d",&data);
+            insert_end(data);
+            break;
+        case 4:
             return 0;
         default:
             printf("invalid choice \n");

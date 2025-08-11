6a693a5e0051a0f263105586c3de53b13931732a
Bisha-Rai <bishalchamling14@gmail.com>
2025-08-11 09:23:22 +0545

    insert in position 2.0

--- EXAMS/DSA/QUEUE And LINKED LIST/SinglyLinkedList.c
+++ EXAMS/DSA/QUEUE And LINKED LIST/SinglyLinkedList.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+void insert_pos_alternate(int,int);
 struct node
 {
     int info;
@@ -97,9 +98,10 @@ int main()
     printf("enter 1 to insert from beginning\n");
     printf("enter 2 to insert from end\n");
     printf("enter 3 to insert at any position\n");
-    printf("enter 4 to display\n");
+     printf("enter 4 to insert at any position alternative way\n");
+    printf("enter 5 to display\n");
     
-    printf("enter 5 to exit\n");
+    printf("enter 6 to exit\n");
     scanf("%d",&c);
     switch (c)
     {
@@ -108,7 +110,7 @@ int main()
             scanf("%d",&data);
             insert_beginning(data);
             break;
-        case 4:
+        case 5:
             display();
             break;
         case 3:
@@ -123,8 +125,16 @@ int main()
             scanf("%d",&data);
             insert_end(data);
             break;
-        case 5:
+        case 6:
             return 0;
+        case 4:
+            printf("enter the position you want to insert the element is \n");
+            scanf("%d",&pos);
+            printf("enter the data\n");
+            scanf("%d",&data);
+            insert_pos_alternate(data,pos);
+            break;
+
         default:
             printf("invalid choice \n");
             break;
@@ -134,3 +144,37 @@ int main()
 }
 
 
+
+void insert_pos_alternate(int d,int p)
+{   
+    if(p<1)
+    {
+        printf("invalid positon\n");
+        return;
+    }
+    int i;
+    struct node*ptr=head;
+    for(i=1;i<p;i++)
+    {
+        if(ptr==NULL)
+        {
+            printf("invalid position\n");
+            return;
+        }
+        else
+        {
+            ptr=ptr->next;
+        }
+    }
+    if(p==1)
+    {
+        insert_beginning(d);
+        return;
+    }
+    struct node*newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->info=ptr->info;
+    newnode->next=ptr->next;
+    ptr->next=newnode;
+    ptr->info=d;
+
+}
